5cc891debc4020b5879806d855e6963a514c0afd
Roger Chau <RogerChau1999@gmail.com>
2022-09-26 12:40:58 -0400

    q3 updated with comments

--- assignments/week 1/bash exercise/q3.sh
+++ assignments/week 1/bash exercise/q3.sh
@@ -1,10 +1,10 @@
 #1/bin/bash
 read -p "What number would you like to find the factorial of? " f #user inputs a number
 
-fact=1
+fact=1 #set to 1 so when the line in the loop runs, it will be reassigned to the proper number
 
 for (( i = $f; i >= 1; i-- ));
 do
-	fact=$((fact * i));
+	fact=$((fact * i)); #reassigns fact to the number that needs to be multiplied next
 done
 echo $fact
