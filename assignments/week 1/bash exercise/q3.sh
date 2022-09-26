fbfe196d73dc8fc81e71de7a3cdbd25180815393
Roger Chau <RogerChau1999@gmail.com>
2022-09-26 12:36:18 -0400

    q3 updated

--- assignments/week 1/bash exercise/q3.sh
+++ assignments/week 1/bash exercise/q3.sh
@@ -1,4 +1,10 @@
 #1/bin/bash
 read -p "What number would you like to find the factorial of? " f #user inputs a number
 
+fact=1
 
+for (( i = $f; i >= 1; i-- ));
+do
+	fact=$((fact * i));
+done
+echo $fact
