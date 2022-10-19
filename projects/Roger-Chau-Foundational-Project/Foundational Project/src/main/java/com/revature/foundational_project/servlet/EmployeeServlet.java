d099941885fa3757a1b50eeef5095d6983995fd6
Roger Chau <RogerChau1999@gmail.com>
2022-10-19 14:59:17 -0400

    employee login working

--- projects/Roger-Chau-Foundational-Project/Foundational Project/src/main/java/com/revature/foundational_project/servlet/EmployeeServlet.java
+++ projects/Roger-Chau-Foundational-Project/Foundational Project/src/main/java/com/revature/foundational_project/servlet/EmployeeServlet.java
@@ -1,22 +1,27 @@
 package com.revature.foundational_project.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.foundational_project.dao.EmployeeDAO;
+import com.revature.foundational_project.dao.EmployeeDAOImpl;
 import com.revature.foundational_project.models.Employee;
+import com.revature.foundational_project.service.EmployeeServiceAPI;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.time.LocalDateTime;
 
 @WebServlet("/employee")
 public class EmployeeServlet extends HttpServlet {
 
     ObjectMapper mapper = new ObjectMapper();
+    EmployeeDAO ed = new EmployeeDAOImpl();
+    EmployeeServiceAPI esp = new EmployeeServiceAPI();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -32,7 +37,24 @@ public class EmployeeServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("[LOG] - POST request has been received for EmployeeServlet at " + LocalDateTime.now());
-        Employee newEmployee = mapper.readValue(req.getInputStream(), Employee.class);
+        HttpSession session;
+        if (req.getParameter("action").equals("login")){
+            Employee employ = mapper.readValue(req.getInputStream(), Employee.class);
+            Employee empl = esp.login(employ.getUsername(), employ.getPassword());
+            if (empl == null){
+                resp.setStatus(400);
+                resp.getWriter().write("Invalid credentials!");
+            } else{
+                session = req.getSession();
+                session.setAttribute("auth-user", empl);
+                resp.setStatus(200);
+                resp.setContentType("application/json");
+                resp.getWriter().write(mapper.writeValueAsString(empl));
+            }
+        } else if (req.getParameter("action").equals("register")){
+            Employee employ = mapper.readValue(req.getInputStream(), Employee.class);
+            Employee empl = esp.register(employ.getFirst_name(),)
+        }
 
     }
 
